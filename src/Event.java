0ac93fc3b090784f8c66ced8a45dc09a00ab70df
Zhengzhening217230111 <2063860278@qq.com>
2024-03-12 16:17:34 +0800

    Attendee

---
+++ src/Event.java
@@ -0,0 +1,74 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Event {
+private String eventID;
+private String eventName;
+private String eventVenue;
+private LocalDate eventDate;
+ArrayList<Attendee> eventAttendees=new ArrayList<>();
+
+public Event(String eventID,String eventName,String eventVenun,LocalDate eventDate){
+this.eventID=eventID;
+this.eventName=eventName;
+this.eventVenue=eventVenun;
+this.eventDate=eventDate;
+
+this.eventAttendees = new ArrayList<>();
+}
+
+public String getEventID() {
+return eventID;
+}
+
+public void setEventID(String eventID) {
+this.eventID = eventID;
+}
+
+public String getEventName() {
+return eventName;
+}
+
+public void setEventName(String eventName) {
+this.eventName = eventName;
+}
+
+public String getEventVenue() {
+return eventVenue;
+}
+
+public void setEventVenue(String eventVenue) {
+this.eventVenue = eventVenue;
+}
+
+public LocalDate getEventDate() {
+return eventDate;
+}
+
+public void setEventDate(LocalDate eventDate) {
+this.eventDate = eventDate;
+}
+
+public void organizeEvent(int choice) {
+Scanner reader=new Scanner(System.in);
+
+switch (choice) {
+case 1:
+System.out.println("Enter the the Event ID:");
+String id=reader.nextLine();
+setEventID(id);
+
+System.out.println("Enter the name of the Event:");
+String eventName=reader.nextLine();
+setEventName(eventName);
+
+System.out.println("Enter the Event date(YY-MM-DD):");
+LocalDate date=LocalDate.parse(reader.nextLine());
+setEventDate(date);
+
+System.out.println("Enter the venue of the Event:");
+
+}
+}
+}
